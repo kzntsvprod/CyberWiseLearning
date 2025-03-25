f5aa3a818f47541e73344e4d7266483d3648eb4e
kzntsvprod <kuznecovvalentin1@gmail.com>
2025-03-25 17:13:30 +0200

    Компонент Label CyberWiseLearning (v. 0.0.2)

---
+++ frontend/src/components/Label.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "../style/Label.module.css"
+
+const Label = (props) => {
+    return <h1 className={style.title}>{props.title}</h1>
+}
+
+export default Label;
