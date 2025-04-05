2976ec268e81f0aa5fa0f8f285d009d43bd99eb2
kzntsvprod <kuznecovvalentin1@gmail.com>
2025-04-05 18:23:46 +0300

    Зміна моделі користувачів CyberWiseLearning (v. 0.3.2)

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    resetToken: String,
-    resetTokenExpiry: Date,
+    resetToken: { type: String, default: null },
+    resetTokenExpiry: { type: Date, default: null },
 }, { timestamps: true });
 
 export default mongoose.model("User", UserSchema);
