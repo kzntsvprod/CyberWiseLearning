e44958134a61417a127723b6eeff269576396733
kzntsvprod <kuznecovvalentin1@gmail.com>
2025-05-04 19:18:04 +0300

    Компонент попереднього перегляду акаунту CyberWiseLearning (v. 0.8.1)

---
+++ frontend/src/components/AccountPreview.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import style from "../style/AccountPreview.module.css";
+
+const AccountPreview = (props) => {
+    return props.user?.avatar ? (
+        <div className={style.userAvatarCont} onClick={props.handleProfileClick}>
+            <img
+                src={props.user.avatar}
+                alt="User Avatar"
+                className={style.userAvatar}
+            />
+            <span className={props.user.isOnline ? style.onlineDot : style.offlineDot}></span>
+        </div>
+    ) : (
+        <button className={style.iconUser} onClick={props.handleProfileClick}>
+            <span>{props.user?.name?.charAt(0).toUpperCase()}</span>
+            <span className={props.user.isOnline ? style.onlineDot : style.offlineDot}></span>
+        </button>
+    );
+};
+
+export default AccountPreview;
