83562c004e6ed2e2b1bf3bd61354f69549d4deb0
kzntsvprod <kuznecovvalentin1@gmail.com>
2025-05-11 23:53:36 +0300

    Виправлення посилання на зміну пароля 3.0 CyberWiseLearning (v. 0.10.12)

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -143,7 +143,7 @@ export const forgotPassword = async (req, res) => {
             }
         });
 
-        const resetUrl = `${process.env.REACT_APP_CLIENT_URL}/reset-password/${token}`;
+        const resetUrl = "https://cyberwiselearning.onrender.com/reset-password/${token}";
 
         const mailOptions = {
             from: `"CyberWise Learning" <${process.env.GMAIL_USER}>`,
